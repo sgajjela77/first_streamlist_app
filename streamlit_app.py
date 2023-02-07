31e26baf4d5a4ddd6d8207b1bc36a5fe87e451c7
sgajjela77 <100196589+sgajjela77@users.noreply.github.com>
2023-02-07 15:17:24 +0530

    Update streamlit_app.py
    removed space with quotes

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,6 @@ streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_rows)
 
 #Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add? ', ' ')
-add_my_fruit_2 = streamlit.text_input(' ')
+add_my_fruit = streamlit.text_input('What fruit would you like to add? ')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
