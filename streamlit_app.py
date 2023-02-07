f4a6121d34df63f930fecb67b355bd18f6a050cf
sgajjela77 <100196589+sgajjela77@users.noreply.github.com>
2023-02-07 15:20:16 +0530

    Update streamlit_app.py
    text to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_rows)
+streamlit.dataframe(my_data_rows)
 
 #Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add? ')
